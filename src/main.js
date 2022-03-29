037a830d2f30d7550df5ef1d63627ae565be16f6
zhangjian <zhangjian2021@tojoy.com>
2022-03-29 16:37:31 +0800

    feat: 配置项目统一入口, 组件插件化

--- src/main.js
+++ src/main.js
@@ -1,4 +1,16 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+// import { createApp } from 'vue'
+// import App from './App.vue'
 
-createApp(App).mount('#app')
+// createApp(App).mount('#app')
+
+import Button from './components/Button.vue'
+import Header from './components/Header.vue'
+import Page from './components/Page.vue'
+
+export default {
+  install: Vue => {
+    Vue.use(Button);
+    Vue.use(Header);
+    Vue.use(Page);
+  }
+}
