fab3cd3d0468eb14ed4244050fe08c4447135fcd
zhangjian <zhangjian2021@tojoy.com>
2022-05-25 15:00:50 +0800

    feat: 配置typescript

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,7 @@
 /*
  * @Author: zhangjian
  * @Date: 2022-03-29 11:49:58
- * @LastEditTime: 2022-05-23 15:48:09
+ * @LastEditTime: 2022-05-25 14:59:31
  * @LastEditors: zhangjian
  * @Description: 描述
  */
@@ -11,22 +11,22 @@ const path = require("path");
 module.exports = {
   "stories": [
     "../src/**/*.stories.mdx",
-    // "../src/**/*.stories.@(js|jsx|ts|tsx)"
-    "../src/**/*.stories.ts"
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+    // "../src/**/*.stories.ts"
   ],
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials"
   ],
   "framework": "@storybook/vue3",
-  webpackFinal: async (config) => {
-    config.module.rules.push({
-      test: /\.ts$/,
-      loader: "ts-loader",
-      options: { appendTsSuffixTo: [/\.vue$/] },
-    });
-    config.resolve.plugins = config.resolve.plugins || [];
-    config.resolve.plugins.push(new TsconfigPathsPlugin({}));
-    return config;
-  },
+  // webpackFinal: async (config) => {
+  //   config.module.rules.push({
+  //     test: /\.ts$/,
+  //     loader: "ts-loader",
+  //     options: { appendTsSuffixTo: [/\.vue$/] },
+  //   });
+  //   config.resolve.plugins = config.resolve.plugins || [];
+  //   config.resolve.plugins.push(new TsconfigPathsPlugin({}));
+  //   return config;
+  // },
 }
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,10 @@
+/*
+ * @Author: zhangjian
+ * @Date: 2022-03-29 11:49:58
+ * @LastEditTime: 2022-05-25 15:00:05
+ * @LastEditors: zhangjian
+ * @Description: 描述
+ */
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
